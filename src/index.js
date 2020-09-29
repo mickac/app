c72e7d3dd75c0a0a028e1e039529c4860edceaf5
mickac <kaczynski.mp@gmail.com>
2020-09-29 21:33:32 +0100

    Functional Compontents Practice

--- src/index.js
+++ src/index.js
@@ -11,7 +11,22 @@ function MyApp(){
   )
 }
 
+function MyInfo(){
+  return(
+    <div>
+      <h1>Michał Kaczyński</h1>
+      <p>React first steps</p>
+      <h2>Vacation spots:</h2>
+      <ul>
+        <li>United States - Florida</li>
+        <li>Japan - Tokyo</li>
+        <li>Spain - Canarian Isles</li>
+      </ul>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <MyApp />, 
+  <MyInfo />, 
   document.getElementById("root")
 )
