9aafb01cfc9122c2eeacf5ec730ae8faa600fe2e
mickac <kaczynski.mp@gmail.com>
2020-09-29 22:08:04 +0100

    Parent/Child components practice

---
+++ src/components/MainContent.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function MainContent(){
+    return(
+        <div id="main">
+            <h1>List:</h1>
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MainContent
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Navbar(){
+    return(
+        <navbar>This is navbar!</navbar>
+    )
+}
+
+export default Navbar
