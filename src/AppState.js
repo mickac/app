e5289f6ce59fd9cba1a451d80acb0a3c9603030b
mickac <kaczynski.mp@gmail.com>
2020-10-10 23:23:08 +0100

    States practice part1 & part2

--- src/AppState.js
+++ src/AppState.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 class AppState extends React.Component{
+/*
     constructor() {
         super()
         this.state = {
@@ -17,5 +18,47 @@ class AppState extends React.Component{
         )
     }
 }
+*/
+/* practice
+    constructor() {
+        super()
+        this.state = {
+            name: "name name",
+            age: 21
+        }
+    }
 
+    render() {
+        return (
+            <div>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old.</h3>
+            </div>
+        )
+    }
+}
+*/
+// practice part 2
+    constructor() {
+        super()
+        this.state = {
+            //isLoggedIn: true
+            isLoggedIn: false
+        }
+    }
+
+    render() {
+        let login
+        if(this.state.isLoggedIn){
+            login = "in"
+        } else {
+            login = "out"
+        }
+        return (
+            <div>
+                <h1>You are currently logged {login}</h1>
+            </div>
+        )
+    }
+}
 export default AppState
