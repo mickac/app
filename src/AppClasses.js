f1de6d5024876d0dbc238327fc404f83a92b801a
mickac <kaczynski.mp@gmail.com>
2020-10-10 22:51:34 +0100

    Class components practice

--- src/AppClasses.js
+++ src/AppClasses.js
@@ -7,13 +7,57 @@ function AppClasses(){
         </div>
     )
 }
-*/
+
 class AppClasses extends React.Component{
     render(){
         return(
             <div>
                 <p>test</p>
             </div>
+        )
+    }
+}
+*/
+
+//practice
+
+class AppClasses extends React.Component{
+    render(){
+        return(
+            <div>
+                <Header username="user"/>
+                <Greetings />
+            </div>
+        )
+    }
+
+}
+
+class Greetings extends React.Component{
+    render(){
+        const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay
+
+        if(hours < 12) {
+            timeOfDay = "morning"
+        } else if (hours >= 12 && hours < 17) {
+            timeOfDay = "afternoon"
+        } else {
+            timeOfDay = "night"
+        }
+        return(
+            <h1>Good {timeOfDay}!</h1>
+        )
+    }
+}
+
+class Header extends React.Component{
+    render(){
+        return(
+            <header>
+                <p>Welcome, {this.props.username}</p>
+            </header>
         )
     }
 }
