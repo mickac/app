cc8c8bfc528c5f815aa641fc0e43f3e1a4f588fd
mickac <kaczynski.mp@gmail.com>
2020-10-10 23:44:08 +0100

    Todo App Phase 4 - converting function into class and adding states

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import React from "react"
 import Checkbox from "./components/Checkbox"
 import todosData from "./data/todosData"
 
-function App(){
-    const todosContent = todosData.map(todo =>
-        <Checkbox 
-            key = {todo.id}
-            id = {todo.id}
-            text = {todo.text}
-            completed = {todo.completed}
-        />
-    )
-    return(
-        <div className="todo-list">
-            {todosContent}
-        </div>
-    )
+class App extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
+    render(){
+        const todosContent = this.state.todos.map(todo =>
+            <Checkbox 
+                key = {todo.id}
+                id = {todo.id}
+                text = {todo.text}
+                completed = {todo.completed}
+            />
+        )
+        return(
+            <div className="todo-list">
+                {todosContent}
+            </div>
+        )
+    }
+
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 
 //import Promps from "./Promps"
 //import AppClasses from "./AppClasses"
-import AppState from "./AppState"
+//import AppState from "./AppState"
 import App from "./App"
 
-ReactDOM.render(<AppState />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
